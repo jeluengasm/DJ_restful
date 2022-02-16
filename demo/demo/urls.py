b38f1c36983d07a37d7d79eb6eebbce4ff51fe82
Jhon Luengas <jeluengasm@unal.edu.co>
2022-02-16 00:08:27 -0500

    Creating a CreateAPIView subclass

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -6,7 +6,8 @@ from django.urls import path
 import store.views, store.api_views
 
 urlpatterns = [
-    path("api/v1/products/", store.api_views.ProductList.as_view()),
+    path("api/v1/products/", store.api_views.ProductList.as_view()), # URL routing as view for ProductList API
+    path("api/v1/products/new", store.api_views.ProductCreate.as_view()),
     
     path('admin/', admin.site.urls),
     path('products/<int:id>/', store.views.show, name='show-product'),
