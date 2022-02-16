6a3422308e5460c68e3e532eeed7f50853c1c6a6
Jhon Luengas <jeluengasm@unal.edu.co>
2022-02-15 23:54:30 -0500

    Creating a CreateAPIView subclass

--- demo/store/api_views.py
+++ demo/store/api_views.py
@@ -1,9 +1,10 @@
+from xml.dom import ValidationErr
 from django.utils import timezone
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView
 from django_filters.rest_framework import DjangoFilterBackend # Create some filters for the API (according to the attributes selected from the view)
 from rest_framework.filters import SearchFilter # Create a search filter (based on substrings, according to the attributes selected from the view)
 from rest_framework.pagination import LimitOffsetPagination # Allows the pagination of the view
-
+from rest_framework.exceptions import ValidationError # Exception handler to raise in ValidationError
 
 from .serializers import ProductSerializer
 from .models import Product
@@ -16,7 +17,10 @@ class ProductsPagination(LimitOffsetPagination):
     max_limit = 100
 
 
-class ProductList(ListAPIView): # Extends from ListAPIView (only GET request)
+class ProductList(ListAPIView):
+    """ Extends from ListAPIView (only GET request)
+    See doc. in: https://www.django-rest-framework.org/api-guide/generic-views/#listapiview
+    """
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = (DjangoFilterBackend, SearchFilter) # Integrate DjangoFilterBackend, SearchFilter as the filters of the API
@@ -38,3 +42,20 @@ class ProductList(ListAPIView): # Extends from ListAPIView (only GET request)
                     # sale_end__gte = now,
                 )
             return queryset
+        
+
+class ProductCreate(CreateAPIView):
+    """ Used for create-only endpoints.
+        Provides a POST method handler. Doc. in: https://www.django-rest-framework.org/api-guide/generic-views/#createapiview
+    """
+    serializer_class = ProductSerializer
+    
+    def create(self, request, *args, **kwargs):
+        try:
+            price = request.data.get('price')
+            if price is not None and float(price) <= 0.0:
+                raise ValidationError({'price': 'Must be above $0.00.'})
+        except ValueError:
+            raise ValidationError({'price': 'A valid number is required.'})
+        return super().create(request, *args, **kwargs)
+        
