0b99839e0f6229bd7aef7405eaf84d9ea6fc038c
Jhon Luengas <jeluengasm@unal.edu.co>
2022-02-17 23:12:15 -0500

    Serializer that shows model relationships

--- demo/store/serializers.py
+++ demo/store/serializers.py
@@ -1,12 +1,22 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, ShoppingCartItem
 
+class CartItemSerializer(serializers.ModelSerializer):
+    """ Serializer of the model 'CartItemSerializer', who has a Foreign key
+        with 'Product' model.
+    """
+    class Meta:
+        model = ShoppingCartItem
+        fields = ('product', 'quantity')
+        
 class ProductSerializer(serializers.ModelSerializer):
     """ Serialize the class Product """
     
     is_on_sale = serializers.BooleanField(read_only=True)
     current_price = serializers.FloatField(read_only=True)
     description = serializers.CharField(min_length=2, max_length=200)
+    cart_items = serializers.SerializerMethodField()
+    
     class Meta:
         """ Metaclass to setup the serializer """
         # Set the model object to serialize
@@ -16,7 +26,12 @@ class ProductSerializer(serializers.ModelSerializer):
             'id', 'name', 'description', 'price', 'sale_start', 'sale_end',
             'is_on_sale', 'current_price', 
             ) 
-        
+    
+    def get_cart_items(self, instance):
+        """ Getter of 'cart_items' serializer method field. 
+            Note: Getters of the serialized models must be written 'get_<name_of_serializerMethodField>' (cart_items in this case)"""
+        items = ShoppingCartItem.objects.filter(product=instance)
+        return CartItemSerializer(items, many=True).data
     # def to_representation(self, instance):
     #     """ Overrride the serialize representation and add extra fields. """
     #     data = super().to_representation(instance)
